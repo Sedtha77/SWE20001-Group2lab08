e787114bb17d10a50728d68f0ac0ab1dfb973299
Lance3092 <lance3092@gmail.com>
2022-04-06 10:40:05 +1000

    Added initial data classes in commons library

---
+++ src/GoToGre.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GoToGre.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/GoToGre.Common/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToGre.Common
+{
+    class Sale
+    {
+        public int id;
+        public List<Product> SaleItems { get; set; }
+        public Members Customer { get; set; }
+        public DateTime TimeStamp { get; set; }
+        
+    }
+}
